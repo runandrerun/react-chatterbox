9b7f36640fe50500b775e990d8afbacd0ee4ce8f
runandrerun <the.asantiagojr@gmail.com>
2018-12-05 21:26:39 -0500

    Redux and Thunk structure setup

---
+++ src/reducers/chatReducer.js
@@ -0,0 +1,36 @@
+const initialState = {
+  rates: [],
+};
+
+const currencyReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'LOAD_RATES':
+    return {
+      ...state,
+      rates: action.payload.rates,
+    };
+
+    case 'CHOSEN_RATES':
+    return {
+      ...state,
+      chosenRates: action.payload.rates,
+    };
+
+    case 'SELECT_PRIMARY':
+    return {
+      ...state,
+      primaryCurrency: action.payload.primary
+    };
+
+    case 'SELECT_SECONDARY':
+    return {
+      ...state,
+      secondaryCurrency: action.payload.secondary,
+    };
+
+    default:
+    return state;
+  };
+};
+
+export default currencyReducer;
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+// import currencyReducer from './currencyReducer';
+
+
+const rootReducer = combineReducers({
+  currencyState: '',
+})
+
+export default rootReducer;
