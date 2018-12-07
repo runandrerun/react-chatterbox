19bbc8f8e430b8dfb398152988117be35b89123d
runandrerun <the.asantiagojr@gmail.com>
2018-12-06 20:13:36 -0500

    Restructured Redux Actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,74 +1,21 @@
-import { fetchRates, selectBase } from '../adapters/currencyAdapter';
-
 // LOAD DATA
 
-export const loadRates = () => {
+export const loadMessages = () => {
   return (dispatch) => {
     fetchRates()
-      .then(ratesData => {
-        dispatch(setRates(ratesData.rates))
-      })
-  }
-}
+      .then(messagesData => {
+        dispatch(setMessages(messagesData));
+      });
+  };
+};
 
-export const startBase = (primary) => {
-  console.log('startBase', primary)
-  return (dispatch) => {
-    return selectBase(primary)
-      .then(ratesData => {
-        console.log('Second step', ratesData)
-        dispatch(chosenCurrency(ratesData.rates))
-      })
-  }
-}
-
-export const selectPrimaryCurrency = (primary) => {
-  console.log('Inside Selection', primary)
-  return (dispatch) => {
-    dispatch(setPrimaryCurrency(primary))
-  }
-}
-
-export const selectSecondaryCurrency = (secondary) => {
-  return (dispatch) => {
-    dispatch(setSecondaryCurrency(secondary))
-  }
-}
 // SET DATA
 
-const setRates = (rates) => {
-  console.log("Set Rates", rates)
+const setMessages = (messages) => {
   return {
     type: 'LOAD_RATES',
     payload: {
-      rates
-    }
-  }
-}
-
-const setPrimaryCurrency = (primary) => {
-  return {
-    type: 'SELECT_PRIMARY',
-    payload: {
-      primary
-    }
-  }
-}
-
-const setSecondaryCurrency = (secondary) => {
-  return {
-    type: 'SELECT_SECONDARY',
-    payload: {
-      secondary
-    }
-  }
-}
-
-const chosenCurrency = (rates) => {
-    return {
-      type: 'CHOSEN_RATES',
-      payload: {
-        rates
-      }
-    }
-}
+      messages
+    },
+  };
+};
